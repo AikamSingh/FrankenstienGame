66ee735fc17dea86a3b004bc55d8115f6f8e1ab0
Aikam Singh <aikamsingh07@gmail.com>
2023-02-21 15:06:29 -0800

    initial commit

---
+++ frankenstein_game.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env/python3
+
+"""
+frankenstein_game.py
+
+game idea:
+you play the creature
+and choices are based on the plot of Frakenstein
+
+"""
+
+__author__ = "Ava Teng and Aikam Singh"
+__version__ = "2023-02-21"
+
+
+
+def main():
+    print("Welcome to the Frankenstein Choose Your Own Adventure game!")
+    print("-----------------------------------------------------------")
+    print("")
+    
+    
+
+    
+if __name__ == "__main__":
+    main()
+
